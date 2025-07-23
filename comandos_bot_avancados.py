ec347dc73ec2f824fec77145611a1c86d083390f
Bruno123456-del <brunoempreendimento10@gmail.com>
2025-07-23 16:52:18 -0300

    Update comandos_bot_avancados.py

--- comandos_bot_avancados.py
+++ comandos_bot_avancados.py
@@ -34,4 +34,3 @@ def adicionar_comandos_avancados(bot: telebot.TeleBot):
     bot.message_handler(commands=["suporte"])(suporte)
     bot.message_handler(commands=["ranking"])(ranking)
     bot.message_handler(commands=["win"])(postar_win)
-
