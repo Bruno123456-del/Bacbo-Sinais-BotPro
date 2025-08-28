28d1d2e685da500e18602dfd51e9882badecf677
Bruno123456-del <brunoempreendimento10@gmail.com>
2025-08-28 19:24:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ SUPORTE_TELEGRAM = "@Superfinds_bot"
 
 # CORREÇÃO APLICADA: Removido o espaço em 'asctime'
 logging.basicConfig(
-    format="%(asctime )s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
